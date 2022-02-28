07424e38d48d4a16f80b3298180fbb30491358a7
Carlostkd <11921177+carlostkd@users.noreply.github.com>
2022-02-28 14:49:28 +0000

    Update start.py

--- start.py
+++ start.py
@@ -820,7 +820,7 @@ class ToolsConsole:
 
     @staticmethod
     def usage():
-        print(('* Coded By MH_ProDev For Better Stresser\n'
+        print(('* Mass russian attack\n'
                'Note: If the Proxy list is empty, the attack will run without proxies\n'
                '      If the Proxy file doesn\'t exist, the script will download proxies and check them.\n'
                '      Proxy Type 0 = All in config.json\n'
